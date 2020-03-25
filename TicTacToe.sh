58d288ea41ff98446a88130e280728ea49a2a78f
dvranga <devangmranganth>
2020-03-25 22:41:23 +0530

    Displaying Board with Default values

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,5 +1,3 @@
-# As a Player Would like to start fresh with resetting the board
-
 declare -A Board
 function initializeBoard()
 {
@@ -10,23 +8,15 @@ function initializeBoard()
 
 }
 initializeBoard
-echo ${Board[@]}
-#As a Player want to know letter X is Assign to him and Computer is O
-Player=X
-Computer=O
-echo Player assigned as $Player
-echo Computer assigned as $Computer
-
-#Player would like to begin with the toss
-function Toss()
+# Player want to see the Board to choose valid cell to place the Value
+function showBoard()
 {
-	case $1 in
-		1)
-		 Person="Player" ;;
-		*)
-		 Person="Computer" ;;
-	esac
-	echo $Person
+	echo   " ______________"
+	echo " | "${Board[1]}" | " ${Board[2]}" | "${Board[3]}" | "
+	echo   " ______________"
+	echo " | "${Board[4]}" | " ${Board[5]}" | "${Board[6]}" | "
+	echo   " ______________"
+	echo " | "${Board[7]}" | " ${Board[8]}" | "${Board[9]}" | "
+	echo   " ______________"
 }
-Person="$( Toss $(( $RANDOM%2 )) )"
-echo $Person Play First
+showBoard
