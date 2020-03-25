154d274bbf02990eab2856f39e6861fc2ef8dcb1
dvranga <devangmranganth>
2020-03-25 21:59:50 +0530

    Toss to Play the which Person to play

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,19 +1,15 @@
 # As a Player Would like to start fresh with resetting the board
-
-declare -A Board
-function initializeBoard()
-{
-	for (( i=1; i<10; i++ ))
-	do
-		Board[$i]=0;
-	done
-
-}
-initializeBoard
-echo ${Board[@]}
 #As a Player want to know letter X is Assign to him and Computer is O
-Player=X
-Computer=O
-echo Player assigned as $Player
-echo Computer assigned as $Computer
-
+#Player would like to begin with the toss
+function Toss()
+{
+	case $1 in
+		1)
+		 Person="Player" ;;
+		*)
+		 Person="Computer" ;;
+	esac
+	echo $Person
+}
+Person="$( Toss $(( $RANDOM%2 )) )"
+echo Person=$Person
